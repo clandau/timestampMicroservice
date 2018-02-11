79e37b5945b59bdffd59231fbc64a393e94f7f5a
clandau <so.super@gmail.com>
2018-02-11 14:49:56 -0500

    initial commit timestamp microservice

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//cors enables remote testing by FCC
+const cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));
+
+//render static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendfile(__dirname + '/views/index.html');
+});
+
+app.get('/api/timestamp/:date_string?', (req, res) => {
+    let date;
+    let dateString = req.params.date_string;
+    if(!dateString) date = new Date();
+    else {
+      if(dateString.indexOf('-') !== -1) date = new Date(dateString);
+      else date = new Date(parseInt(dateString));
+    }
+    if(!date.getDate()) {
+      res.json({"error" : "Invalid Date" });
+    }
+    else res.json({'unix': date.getTime(), 'utc': date.toUTCString()});
+  });
+
+  const listener = app.listen(PORT, () => {
+      console.log('You are listening on port ' + PORT);
+  });
